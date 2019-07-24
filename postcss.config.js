9e37c94fd775c9c275efc484b872eb40d68aabf4
Domenico Maisano <dev.dmaisano@gmail.com>
2019-07-24 16:20:49 -0400

    fixed postcss config for dev

--- postcss.config.js
+++ postcss.config.js
@@ -2,25 +2,27 @@ const tailwind = require('tailwindcss');
 const purgecss = require('@fullhuman/postcss-purgecss');
 const cssnano = require('cssnano');
 
+const purgecssConfig = purgecss({
+  content: ['./src/**/*.html'],
+  extractors: [
+    {
+      extractor: class TailWindExtractor {
+        static extract(content) {
+          return content.match(/[A-z0-9-:\/]+/g) || [];
+        }
+      },
+      extensions: ['css', 'html'],
+    },
+  ],
+});
+
 module.exports = {
   plugins: [
     tailwind('./tailwind.config.js'),
     cssnano({
       preset: 'default',
     }),
-    purgecss({
-      content: ['./src/**/*.html'],
-      extractors: [
-        {
-          extractor: class TailWindExtractor {
-            static extract(content) {
-              return content.match(/[A-z0-9-:\/]+/g) || [];
-            }
-          },
-          extensions: ['css', 'html'],
-        },
-      ],
-    }),
+    ...(process.env.NODE_ENV === 'production' ? [purgecssConfig] : []),
     require('autoprefixer'),
   ],
 };
